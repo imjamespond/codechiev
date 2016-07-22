e9d8f6e21cab86ca7a352df536129c35d370b330
james <imjamespond@gmail.com>
2016-07-22 17:02:19 +0800

    aaa

--- make.sh
+++ make.sh
@@ -15,20 +15,13 @@ cmake . ../$PROJECT \
 -DROOT=$2 
 make && make install
 
-#./build/server
-#./build/server 2>&1 > log.txt &
 cd $PROJECT_PATH/build/base/tests
 ctest --output-on-failure .
-}
-
-function_git()
-{
 }
 
 if [ "$1" = "build" ]; then
 	function_build ~/cpp/boost_1_61 `pwd`
 elif [ "$1" = "svn" ]; then
-	function_git
 else
 	echo "input build for build,\
 zip for zip."
