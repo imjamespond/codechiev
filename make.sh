5f2e239a73bec5479915f400944f88defdef3232
james <imjamespond@gmail.com>
2016-07-22 17:04:53 +0800

    git pull

--- make.sh
+++ make.sh
@@ -8,18 +8,23 @@ PROJECT_PATH=`pwd`
 
 function_build()
 {
-mkdir -p build
-cd build/
-cmake . ../$PROJECT \
--DBOOST_ROOT=$1 \
--DROOT=$2 
-make && make install
+	mkdir -p build
+	cd build/
+	cmake . ../$PROJECT \
+	-DBOOST_ROOT=$1 \
+	-DROOT=$2 
+	make && make install
 
-cd $PROJECT_PATH/build/base/tests
-ctest --output-on-failure .
+	cd $PROJECT_PATH/build/base/tests
+	ctest --output-on-failure .
+}
+function_update()
+{
+	git pull origin master
 }
 
 if [ "$1" = "build" ]; then
+	function_update
 	function_build ~/cpp/boost_1_61 `pwd`
 elif [ "$1" = "svn" ]; then
 else
