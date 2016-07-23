1ffcf4bb86048e2442cbf384f809232ed0d42f3b
james <imjamespond@gmai.com>
2016-07-23 15:16:22 +0800

    aaa

--- make.sh
+++ make.sh
@@ -23,12 +23,20 @@ function_update()
 	git pull origin master
 }
 
+function_commitpush
+{
+	git commit -m 'cm'
+	git push local master
+}
+
 if [ "$1" = "build" ]; then
 	function_update
 	function_build ~/cpp/boost_1_61_0 `pwd`
 elif [ "$1" = "bui" ]; then
 	function_update
 	function_build ~/boost_1_59 `pwd`
+elif [ "$1" = "cop" ]; then
+	function_commitpush
 else
 	echo "input build for build,\
 zip for zip."
