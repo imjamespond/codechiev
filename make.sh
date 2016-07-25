93a8f66ecd09e8a1a71df0bc3de2ef1651db2bed
james <imjamespond@gmai.com>
2016-07-25 22:24:05 +0800

    cm

--- make.sh
+++ make.sh
@@ -27,14 +27,6 @@ function_update()
 	git pull origin master
 }
 
-function_commitpush()
-{
-	git commit -a -m 'cm'
-	git push local master
-	ssh james@127.0.0.1 "cd ~/cpp/codechiev;
-	./make.sh bui"
-}
-
 if [ "$1" = "build" ]; then
 	function_update
 	function_build ~/cpp/boost_1_61_0 `pwd`
@@ -42,12 +34,15 @@ elif [ "$1" = "bui" ]; then
 	function_update
 	function_build ~/boost_1_59 `pwd`
 elif [ "$1" = "cop" ]; then
-	function_commitpush
+	git commit -a -m 'cm'
+	git push local master
+	ssh james@127.0.0.1 "cd ~/cpp/codechiev;
+	./make.sh bui"
 elif [ "$1" = "coph" ]; then
-        git commit -a -m 'cm'
-        git push origin master
-        ssh -p9726 web1@yuyouidea.com "cd ~/cpp/codechiev;
-        ./make.sh build"
+	git commit -a -m 'cm'
+	git push origin master
+	ssh -p9726 web1@yuyouidea.com "cd ~/cpp/codechiev;
+	./make.sh build"
 else
 	echo "input build for build,\
 zip for zip."
