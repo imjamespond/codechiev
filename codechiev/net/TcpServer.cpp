4f6a8899a91cb3e7bc13965afa6e58c0b3027e11
james <imjamespond@gmai.com>
2016-07-26 21:07:35 +0800

    cm

--- codechiev/net/TcpServer.cpp
+++ codechiev/net/TcpServer.cpp
@@ -79,7 +79,7 @@ TcpServer::pollEvent(const chanenl_vec &vec)
                 if(buffer.writable()<=4)
                 {
                     LOG_DEBUG<<buffer.str();
-                    buffer.reset();
+                    buffer.readall();
                 }
                 int len = static_cast<int>(::read(channel->getFd(), buffer.data(), 4));
                 if(len)
@@ -89,7 +89,7 @@ TcpServer::pollEvent(const chanenl_vec &vec)
                 if(EAGAIN==errno)
                 {
                     LOG_DEBUG<<buffer.str();
-                    buffer.reset();
+                    buffer.readall();
                     //set channel being interesting in read event
                     channel->setEvent(EPOLLIN);
                     loop_.getPoll().setChannel(channel);
