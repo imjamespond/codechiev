73d4e4c29638e9de8b6b28952aa29a4906a3c5be
james <imjamespond@gmail.com>
2016-07-27 15:00:39 +0800

    cm

--- codechiev/net/TcpClient.cpp
+++ codechiev/net/TcpClient.cpp
@@ -16,7 +16,7 @@ using namespace codechiev::net;
 
 TcpClient::TcpClient(const std::string& ip, uint16_t port):
 TcpEndpoint(ip, port),
-loop_(boost::bind(&TcpServer::pollEvent, this, _1))
+loop_(boost::bind(&TcpClient::pollEvent, this, _1))
 {
     
 }
@@ -36,4 +36,57 @@ void
 TcpClient::close()
 {
     
+}
+
+#undef UseEpollET
+void
+TcpClient::pollEvent(const chanenl_vec &vec)
+{
+    
+    for( chanenl_vec::const_iterator it=vec.begin();
+        it!=vec.end();
+        it++)
+    {
+        net::Channel *channel = *it;
+        if (channel->getFd() == channel_.getFd())
+        {
+            onConnect(channel);
+        }else
+        {
+            if(channel->getEvent() & EPOLLIN)
+            {
+                onRead(channel);
+            }else if(channel->getEvent() & EPOLLOUT)
+            {
+                onWrite(channel);
+            }else if(channel->getEvent() & (EPOLLHUP|EPOLLRDHUP) )
+            {
+                onClose(channel);
+            }
+        }
+    }//for
+}
+
+void
+TcpClient::onConnect(Channel* channel)
+{
+    socklen_t socklen = addr_.socklen;
+    InetAddress addr;
+    int connfd = ::accept(channel_.getFd(),
+                          (struct sockaddr *) &addr.sockaddrin, &socklen);
+    if (connfd == -1) {
+        LOG_ERROR<<("accept");
+        return;
+    }
+    channel_ptr connsock(new Channel(connfd));
+    
+#ifdef UseEpollET
+    connsock->setEvent(EPOLLIN|EPOLLOUT |EPOLLET);
+    LOG_TRACE<<"UseEpollET";
+#else
+    connsock->setEvent(EPOLLIN);
+#endif
+    loop_.getPoll().addChannel(connsock.get());
+    if(onConnect_)
+        onConnect_(connsock.get());
 }
