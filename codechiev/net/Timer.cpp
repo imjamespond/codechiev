664c882ad9cb67bd4503d5cc833cb6949c64110c
james <imjamespond@gmai.com>
2016-07-29 23:26:05 +0800

    cm

--- codechiev/net/Timer.cpp
+++ codechiev/net/Timer.cpp
@@ -99,6 +99,7 @@ Scheduler::unscheduleTimer(int fd)
 void
 Scheduler::pollEvent(const channel_vec& vec)
 {
+    LOG_TRACE<<"pollEvent:"<<channel->getFd();
     for(channel_vec::const_iterator it=vec.begin();
         it!=vec.end();
         it++)
@@ -109,7 +110,7 @@ Scheduler::pollEvent(const channel_vec& vec)
         ssize_t len = ::read(channel->getFd(), &data, sizeof(data_t));
         if(len==sizeof(data_t))
         {
-            LOG_TRACE<<"pollEvent:"<<channel->getFd();
+
             timer_map::const_iterator it = timers_.find( channel->getFd());
             if(it!=timers_.end())
             {
