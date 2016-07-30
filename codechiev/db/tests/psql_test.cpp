46eaa281ea7a92e6f93475e730a14a72d2f510f6
james <imjamespond@gmai.com>
2016-07-30 13:06:18 +0800

    cm

--- codechiev/db/tests/psql_test.cpp
+++ codechiev/db/tests/psql_test.cpp
@@ -8,8 +8,58 @@
 
 #include <db/orm-psql.h>
 #include <db/Psql.h>
-using namespace codechiev;
-int main(int argc, const char * argv[]) {
+#include <vector>
+#include <boost/shared_ptr.hpp>
+namespace codechiev{
+    namespace db{
+        //DECLEAR_TABLE( Student, student, ((int,id,id))((std::string,name,name))((std::string,sex,name)))
+        DECLEAR_PSQL_TABLE( User, user2, 2, ((std::string,uname,username))((int,gender,gender))\
+                           ((int64_t,createdate,createdate)))
 
+        class UserEx:public User<PSql>
+        {
+        public:
+            void selectByName(const std::string& name);
+            static void selectListByGender(int32_t , User::vec_type&);
+        };
+
+        void
+        UserEx::selectByName(const std::string &name)
+        {
+            const char *sql = "SELECT * FROM user2 WHERE username = $1";
+            const char *paramValues[1];
+            int paramLengths[1];
+            int paramFormats[1];
+            paramValues[0] = name.c_str();
+            paramLengths[0] = static_cast<int>(name.length());
+            paramFormats[0] = 1;
+
+            PSql::Result result = PSql::select(sql, 1, paramValues, paramLengths, paramFormats, 1);
+            this->assemble(result);
+        }
+
+        void
+        UserEx::selectListByGender(int32_t gender, User::vec_type& userVec)
+        {
+            const char *sql = "SELECT * FROM user2 WHERE gender = $1";
+            const char *paramValues[1];
+            int paramLengths[1];
+            int paramFormats[1];
+            uint32_t val = hostToNetworkInt32(gender);
+            paramValues[0] = (char *) &val;
+            paramLengths[0] = sizeof(val);
+            paramFormats[0] = 1;
+
+            PSql::Result result = PSql::select(sql, 1, paramValues, paramLengths, paramFormats, 1);
+            User::assembleVector(result, userVec);
+        }
+    }
+}
+
+using namespace codechiev;
+int main(int argc, const char * argv[])
+{
+    db::UserEx user;
+    user.selectById(10093);
     return 0;
 }
