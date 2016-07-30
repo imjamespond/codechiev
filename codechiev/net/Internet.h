d3b5e1560b0c1eb7b29d5eb09333ef28f2dcd23b
james <imjamespond@gmai.com>
2016-07-30 12:34:50 +0800

    cm

--- codechiev/net/Internet.h
+++ codechiev/net/Internet.h
@@ -15,6 +15,11 @@
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 
+#ifdef __linux__
+#define htonll(x) ((1==htonl(1)) ? (x) : ((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
+#define ntohll(x) ((1==ntohl(1)) ? (x) : ((uint64_t)ntohl((x) & 0xFFFFFFFF) << 32) | ntohl((x) >> 32))
+#endif // __linux__
+
 namespace codechiev {
     namespace net {
 
