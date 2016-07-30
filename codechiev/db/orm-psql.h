1843ae9478fcfc7e31d18c939e5d62d74f7a5822
james <imjamespond@gmai.com>
2016-07-30 16:37:30 +0800

    cm

--- codechiev/db/orm-psql.h
+++ codechiev/db/orm-psql.h
@@ -51,7 +51,7 @@ public:\
 };\
 template <> inline void clazz<PSql>::update()\
 {\
-    const char* sql = "update " #table " set id=id " BOOST_PP_SEQ_FOR_EACH_I( PSQL_UPDATE, , member_seq) " where id=$" BOOST_PP_STRINGIZE( 1 );\
+    const char* sql = "update " #table " set id=id " BOOST_PP_SEQ_FOR_EACH_I( PSQL_UPDATE, , member_seq) " where id=$" ;\
     const char *val[param_num];\
     LOG_INFO<<BOOST_PP_TUPLE_SIZE(member_seq);\
     int         len[param_num];\
