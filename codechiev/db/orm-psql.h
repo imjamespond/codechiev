94dddf3b4ee313936b46e66e053f91c2b85e32bd
james <imjamespond@gmai.com>
2016-07-30 11:49:48 +0800

    add db

---
+++ codechiev/db/orm-psql.h
@@ -0,0 +1,7 @@
+#ifndef ORM-PSQL_H_INCLUDED
+#define ORM-PSQL_H_INCLUDED
+
+#include "orm.h"
+
+
+#endif // ORM-PSQL_H_INCLUDED
---
+++ codechiev/db/tests/psql_test.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  codechiev
+//
+//  Created by metasoft on 16/7/15.
+//  Copyright © 2016年 metasoft. All rights reserved.
+//
+
+using namespace codechiev;
+#include <db/orm-psql.h>
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
