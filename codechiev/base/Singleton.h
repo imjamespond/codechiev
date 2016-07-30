3426bc20486a6f1f2a542f58f60c9aca4d99f348
james <imjamespond@gmai.com>
2016-07-30 13:44:13 +0800

    cm

--- codechiev/base/Singleton.h
+++ codechiev/base/Singleton.h
@@ -14,13 +14,14 @@
 
 #else
 #include <pthread.h>
+#include <stdlib.h>
 #endif
 
 namespace codechiev
 {
     namespace base
     {
-        
+
 #ifdef WIN32
         template <class T>
         class Singleton
@@ -31,20 +32,20 @@ namespace codechiev
                 InitOnceExecuteOnce(&once_, &Singleton::initRoutine, NULL, NULL);
                 return t_;
             }
-            
+
             static BOOL CALLBACK initRoutine(PINIT_ONCE InitOnce, PVOID Parameter, PVOID *lpContex)
             {
                 t_ = new T;
                 ::atexit(&Singleton::destructor);
                 return TRUE;
             }
-            
+
             static void destructor()
             {
                 delete t_;
                 t_ = NULL;
             }
-            
+
         protected:
         private:
             static T* t_;
@@ -64,19 +65,19 @@ namespace codechiev
                 pthread_once(&once_, &Singleton::initRoutine);
                 return t_;
             }
-            
+
             static void initRoutine()
             {
                 t_ = new T;
                 ::atexit(&Singleton::destructor);
             }
-            
+
             static void destructor()
             {
                 delete t_;
                 t_ = NULL;
             }
-            
+
         protected:
         private:
             static T* t_;
@@ -87,7 +88,7 @@ namespace codechiev
         template <class T>
         T* Singleton<T>::t_ = NULL;
 #endif
-        
+
     }
 }
 
