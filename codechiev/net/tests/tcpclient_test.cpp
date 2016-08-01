c16c880d0386884bc238abd729b7b25fd91f83ef
james <imjamespond@gmai.com>
2016-08-02 00:45:17 +0800

    cm

--- codechiev/net/tests/tcpclient_test.cpp
+++ codechiev/net/tests/tcpclient_test.cpp
@@ -40,7 +40,7 @@ public:
 
     void connectall()
     {
-        for(int i=0; i<100; i++)
+        for(int i=0; i<10000; i++)
         {
             channel_ptr chn = connect();
             if(chn)
