7cf4afe05c4b69afc6f650edc9502ea66747ff86
james <imjamespond@gmail.com>
2016-08-10 17:06:36 +0800

    cm

--- codechiev/utility/tests/openssl_test.cpp
+++ codechiev/utility/tests/openssl_test.cpp
@@ -18,6 +18,11 @@
 using namespace codechiev::base;
 using namespace codechiev::utility;
 
+/*
+ openssl genrsa -out private.pem 2048
+ openssl rsa -in private.pem -outform PEM -pubout -out public.pem
+ */
+
 
 const int kNum=9999;
 const int kThread=4;
