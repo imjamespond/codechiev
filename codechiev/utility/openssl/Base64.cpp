7b918116d153a16a7784abb5455483238d01dd08
james <imjamespond@gmail.com>
2016-08-11 11:41:09 +0800

    cm

--- codechiev/utility/openssl/Base64.cpp
+++ codechiev/utility/openssl/Base64.cpp
@@ -20,27 +20,42 @@ Base64::Base64Encode(const unsigned char* data, size_t length)
     //since binary string must of contained a NULL,so we need the excact length
 
     //Encodes a binary safe base 64 string
-    BIO *bio, *bmem, *b64;
+    /*BIO *bio, *b64;
     BUF_MEM *bufferPtr;
 
     b64 = BIO_new(BIO_f_base64());
     bmem = BIO_new(BIO_s_mem());
-    bio = BIO_push(b64, bmem);
+    bio = BIO_push(b64, bio);
 
+   
     BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Ignore newlines - write everything in one line
     BIO_write(bio, data, static_cast<int>(length));
     BIO_flush(bio);
     BIO_get_mem_ptr(bio, &bufferPtr);
     BIO_set_close(bio, BIO_NOCLOSE);
-
-    int len = static_cast<int>(bufferPtr->length);
-    char* encodedData = static_cast<char *>(::malloc(len+1));
-    ::memcpy(encodedData, bufferPtr->data, len);
-    encodedData[len] = '\0';//need a terminator for http?
-    std::string encoded(encodedData);
-    ::free(encodedData);
-    BIO_free_all(bio);
-    BIO_free_all(bmem);
+     
+     
+     int len = static_cast<int>(bufferPtr->length);
+     char* encodedData = static_cast<char *>(::malloc(len+1));
+     ::memcpy(encodedData, bufferPtr->data, len);
+     encodedData[len] = '\0';//need a terminator for http?
+     
+     */
+    
+    BIO *b64 = BIO_new(BIO_f_base64()); // create BIO to perform base64
+    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+    BIO *mem = BIO_new(BIO_s_mem()); // create BIO that holds the result
+    // chain base64 with mem, so writing to b64 will encode base64 and write to mem.
+    BIO_push(b64, mem);
+    BIO_write(b64, data, static_cast<int>(length));
+    BIO_flush(b64);
+    BUF_MEM *bufferPtr;
+    BIO_get_mem_ptr(b64, &bufferPtr);
+    BIO_set_close(b64, BIO_NOCLOSE);
+    
+    std::string encoded;
+    encoded.append(bufferPtr->data, static_cast<int>(bufferPtr->length));
+    
     BIO_free_all(b64);
 
     return encoded; //success
@@ -58,8 +73,6 @@ Base64::Base64Decode(const char* b64message, unsigned_char_vec_t& vec)
 {
     //Decodes a base64 encoded string
     BIO *bio, *b64;
-
-    unsigned char buffer[16]={};
     int len(0),rtLen(0);
 
     len = static_cast<int>(::strlen(b64message));
@@ -71,6 +84,7 @@ Base64::Base64Decode(const char* b64message, unsigned_char_vec_t& vec)
     b64 = BIO_new(BIO_f_base64());
     bio = BIO_push(b64, bio);
 
+    unsigned char buffer[16]={};
     BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Do not use newlines to flush buffer
     while((len = BIO_read(bio, buffer, 16)))
     {
