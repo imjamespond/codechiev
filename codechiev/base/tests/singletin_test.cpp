17bffd5448b708317cb1af747b72f408c1f124a5
james <imjamespond@gmail.com>
2016-08-11 14:42:54 +0800

    singleton test

---
+++ codechiev/base/tests/singletin_test.cpp
@@ -0,0 +1,69 @@
+//
+//  main.cpp
+//  codechiev
+//
+//  Created by metasoft on 16/7/15.
+//  Copyright © 2016年 metasoft. All rights reserved.
+//
+
+#include <stdio.h>
+#include <boost/bind.hpp>
+#include <errno.h>
+#include <exception>
+
+#include <base/BlockingQueue.hpp>
+#include <base/Logger.hpp>
+#include <base/AtomicNumber.h>
+#include <base/Condition.hpp>
+#include <base/Singleton.h>
+
+using namespace codechiev::base;
+
+class TestSingleton
+{
+public:
+    TestSingleton(){count.addAndFetch(1);}
+    
+    static AtomicNumber<int> count;
+};
+class TestThreadSingleton
+{
+public:
+    TestThreadSingleton(){count.addAndFetch(1);}
+
+    static AtomicNumber<int> count;
+};
+
+const int kNumber = 99999;
+CountLatch latch(kNumber);
+void SingletonTest()
+{
+    Singleton<TestSingleton>::get();
+    latch.reduce(1);
+}
+void ThreadSingletonTest()
+{
+    ThreadSingleton<TestThreadSingleton>::get();
+}
+
+AtomicNumber<int> TestSingleton::count(0);
+AtomicNumber<int> TestThreadSingleton::count(0);
+
+typedef BlockingQueue<10> bq;
+bq queue;
+
+int main(int argc, const char * argv[]) {
+    
+    BlockingQueue<10> queue;
+    queue.commence();
+    
+    for(int i=0; i<kNumber; i++)
+    {
+        queue.addJob(boost::bind(&SingletonTest));
+        queue.addJob(boost::bind(&ThreadSingletonTest));
+    }
+    
+    latch.latch();
+    queue.stop();
+    return 0;
+}
