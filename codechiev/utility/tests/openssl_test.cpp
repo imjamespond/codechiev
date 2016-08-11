28f7ca615b48612b1eba37ce298ebf6b907fcf03
james <imjamespond@gmail.com>
2016-08-11 13:34:33 +0800

    cm

--- codechiev/utility/tests/openssl_test.cpp
+++ codechiev/utility/tests/openssl_test.cpp
@@ -39,21 +39,23 @@ void print()
         return ;
     }
     RsaUtil rsautil;
-
-    unsigned_char_vec encrytedPasswd;
-    unsigned_char_vec decryptedPasswd;
-    unsigned_char_vec decryptedBase64;
-
-    int encryptLength  = \
-    rsautil.publicPemEncrypt("foobar", encrytedPasswd);
-    std::string base64Passwd = Base64::Base64Encode(encrytedPasswd.data(), encryptLength);
-    //LOG_INFO<<"publicPemEncrypt>>>>>>>>>>>>>>>>>>>:"<<reinterpret_cast<const char*>(encrytedPasswd.data());
-    LOG_INFO<<"Base64Encode>>>>>>>>>>>>>>>>>>>>>>>:"<<base64Passwd;
-    Base64::Base64Decode(base64Passwd.c_str(), decryptedBase64);
-    rsautil.privatePemDecrypt(decryptedBase64.data(), decryptedBase64.size(), decryptedPasswd);
-    LOG_INFO<<"privatePemDecrypt<<<<<<<<<<<<<<<<<<<<<:"<<reinterpret_cast<const char*>(decryptedPasswd.data())<< " size:"<< (int)decryptedPasswd.size()<<"\n\n\n\n\n\n";
-    assert(decryptedPasswd.size()==7);/**/
-
+    
+    for(int i=0; i<9999; i++)
+    {
+        unsigned_char_vec encrytedPasswd;
+        unsigned_char_vec decryptedPasswd;
+        unsigned_char_vec decryptedBase64;
+        
+        int encryptLength  = \
+        rsautil.publicPemEncrypt("foobar", encrytedPasswd);
+        std::string base64Passwd = Base64::Base64Encode(encrytedPasswd.data(), encryptLength);
+        //LOG_INFO<<"publicPemEncrypt>>>>>>>>>>>>>>>>>>>:"<<reinterpret_cast<const char*>(encrytedPasswd.data());
+        //LOG_INFO<<"Base64Encode>>>>>>>>>>>>>>>>>>>>>>>:"<<base64Passwd;
+        Base64::Base64Decode(base64Passwd.c_str(), decryptedBase64);
+        rsautil.privatePemDecrypt(decryptedBase64.data(), decryptedBase64.size(), decryptedPasswd);
+        //LOG_INFO<<"privatePemDecrypt<<<<<<<<<<<<<<<<<<<<<:"<<reinterpret_cast<const char*>(decryptedPasswd.data())<< " size:"<< (int)decryptedPasswd.size()<<"\n\n\n\n\n\n";
+        assert(decryptedPasswd.size()==7);/**/
+    }
 }
 
 int main(int argc, const char * argv[]) {
@@ -61,10 +63,7 @@ int main(int argc, const char * argv[]) {
     Time began = Time::NowTm();
     
     queue.commence();
-    for(int i=0; i<9999; i++)
-    {
-        queue.addJob(boost::bind(&print));
-    }
+    queue.addJob(boost::bind(&print));
 
     Time::SleepMillis(3000l);
     Time now = Time::NowTm();
