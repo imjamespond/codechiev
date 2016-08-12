0d0be856450b6e40843691549481b35e4e8fddb9
james <imjamespond@gmail.com>
2016-08-12 14:15:55 +0800

    cm

--- codechiev/db/orm-psql.h
+++ codechiev/db/orm-psql.h
@@ -24,7 +24,7 @@ public:\
 typedef typename ISql::Result result_type;\
 typedef boost::shared_ptr<clazz > ptr_type;\
 typedef std::vector<ptr_type > vec_type;\
-void selectById(int64_t);\
+void find();\
 \
 public:\
     Field<int64_t> id;\
@@ -32,12 +32,11 @@ public:\
     const static int param_size = BOOST_PP_SEQ_SIZE(member_seq); \
     const static int param_num = 1 + param_size; \
 };\
-template <> void clazz<PSql>::selectById(int64_t argId)\
+template <> void clazz<PSql>::find()\
 {\
 const char* sql = "select * from " #table " where id=$1";\
 PSql::Result result;\
-PSql::selectById(result, sql, argId);\
-id.toField(result.getFieldValOfRow("id", 0));\
+PSql::selectById(result, sql, id.getValue());\
 BOOST_PP_SEQ_FOR_EACH( TABLE_ASSIGN, 0, member_seq)\
 result.freeAll();\
 }
