d91439f2e4275e81944769844c7d47a888c813dd
james <imjamespond@gmail.com>
2016-08-16 15:29:46 +0800

    cm

--- codechiev/net/protobuf/protoServer.cpp
+++ codechiev/net/protobuf/protoServer.cpp
@@ -78,8 +78,7 @@ ProtoServer::onMessage(const std::string& msg, int fd)
         LOG_INFO<<msgPtr->DebugString();
         Message *rsp = service.GetResponsePrototype(method).New();
         channel_ptr channel = getChannel(fd);
-        rpc_send_func sendfunc = boost::bind(&ProtoServer::send, this, _1, _2);
-        PbRpcChannel *rpcchannel = new PbRpcChannel(channel, sendfunc);
+        PbRpcChannel *rpcchannel = new PbRpcChannel(channel, boost::bind(&ProtoServer::send, this, _1, _2));
         Closure* callback = NewCallback(&PbRpcChannel::Callback, rpcchannel, rsp);
         service.CallMethod(method, NULL, msgPtr.get(), rsp, callback);
     }
