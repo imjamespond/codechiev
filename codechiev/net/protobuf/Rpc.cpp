6e2b0a45865ec43323f0c256ea390fcd36111618
james <imjamespond@gmail.com>
2016-08-16 12:34:02 +0800

    cm

--- codechiev/net/protobuf/Rpc.cpp
+++ codechiev/net/protobuf/Rpc.cpp
@@ -19,7 +19,7 @@ void PbRpcChannel::CallMethod(
         if(channel_ptr c = channel_.lock())
         {
             GenericReq req;
-            req.set_method( method->full_name());
+            req.set_method( method->name());
             req.set_request( request->SerializeAsString());
             
             std::string serialized = req.SerializeAsString();
