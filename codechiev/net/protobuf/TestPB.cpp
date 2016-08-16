3395829dbdbce94a8e178c8f6aa3fa654e5c6c98
james <imjamespond@gmail.com>
2016-08-16 15:35:03 +0800

    cm

--- codechiev/net/protobuf/TestPB.cpp
+++ codechiev/net/protobuf/TestPB.cpp
@@ -15,5 +15,8 @@ void
 TestServiceImpl::RpcTest(RpcController *controller, const TestRequest *request, GenericRsp *response, Closure *done)
 {
     LOG_INFO<<"TestServiceImpl::RpcTest";
+    response->set_ok(true);
+    response->set_msg("RpcTest Ok!");
+    
     done->Run();
 }
