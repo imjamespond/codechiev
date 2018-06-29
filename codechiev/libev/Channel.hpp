075cd3297802ea40e04ba94eeb5e6310d3406446
james <james_huangjian@163.com>
2018-06-29 17:58:09 +0800

    ..

---
+++ codechiev/libev/Channel.hpp
@@ -0,0 +1,22 @@
+#ifndef Channel_hpp
+#define Channel_hpp
+
+namespace codechiev
+{
+namespace libev
+{
+
+class Channel 
+{
+  public:
+    explicit Channel();
+    ~Channel();
+
+    
+  public:
+};
+
+} // namespace libev
+} // namespace codechiev
+
+#endif /* Channel_hpp */
